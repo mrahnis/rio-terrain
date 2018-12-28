3a635dced4a2fd51f5d49c0b63d2896521db05b5
Mike Rahnis <mike.rahnis@gmail.com>
2018-12-28 00:25:51 -0500

    Update mocks in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy', 'rasterio', 'matplotlib', 'scipy']
+MOCK_MODULES = ['numpy', 'rasterio', 'matplotlib', 'scipy', 'crick']
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
