1d46279a739ec3da40e762f9bc220cd4410cb675
Mike Rahnis <mike.rahnis@gmail.com>
2018-12-28 23:06:03 -0500

    Try to get autodoc to work

--- terrain/__init__.py
+++ terrain/__init__.py
@@ -1,6 +1,3 @@
-
-import logging
-
 from terrain.core.terrain import *
 from terrain.core.statistics import *
 from terrain.core.windowing import *
@@ -8,5 +5,7 @@ from terrain.core.windowing import *
 __author__ = "Mike Rahnis"
 __version__ = '0.0.0'
 
-logger = logging.getLogger(__name__)
 
+import logging
+
+logger = logging.getLogger(__name__)
