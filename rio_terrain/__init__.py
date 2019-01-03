364e2ba50714b616003c36825caeb8cbc71c18c3
Mike Rahnis <mike.rahnis@gmail.com>
2019-01-03 13:29:48 -0500

    Prepare to tag 0.0.5

--- rio_terrain/__init__.py
+++ rio_terrain/__init__.py
@@ -5,7 +5,7 @@ from rio_terrain.core.statistics import *
 from rio_terrain.core.windowing import *
 
 __author__ = "Mike Rahnis"
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 
 logger = logging.getLogger(__name__)
