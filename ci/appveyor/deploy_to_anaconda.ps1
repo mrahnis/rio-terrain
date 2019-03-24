a4d1efa9c5c5a9bb7015abbb270df8a8c7a7ba9a
Mike Rahnis <mike.rahnis@gmail.com>
2019-03-23 23:34:03 -0400

    Don't forget to update the conda meta.yml

--- ci/appveyor/deploy_to_anaconda.ps1
+++ ci/appveyor/deploy_to_anaconda.ps1
@@ -1,19 +1,32 @@
-if (($Env:APPVEYOR_REPO_TAG -eq "true") -and
+If (($Env:APPVEYOR_REPO_TAG -eq "true") -and
     ($Env:APPVEYOR_REPO_NAME -eq ${Env:GITHUB_REPO_NAME})) {
-  $tar_glob = ".\conda-bld\noarch\${Env:PYPKG}-${Env:APPVEYOR_REPO_TAG_NAME}-*.tar.bz2";
-  Write-Host "distribution file: $tar_glob";
-  if ($env:APPVEYOR_REPO_TAG_NAME.StartsWith("v")) {
+
+  $file_to_upload = (conda build --output .) | Out-String
+
+  #$tar_glob = ".\conda-bld\noarch\${Env:PYPKG}-${Env:APPVEYOR_REPO_TAG_NAME}-*.tar.bz2";
+  #Write-Host "distribution file: $tar_glob";
+
+  #If (Test-Path $file_to_upload -PathType Leaf) {
+  #    Write-Host "Uploading $file_to_upload"
+  #} Else {
+  #    Write-Host "Unable to find file to upload: $file_to_upload" -ForegroundColor Red
+  #    Return
+  #}
+
+  If ($env:APPVEYOR_REPO_TAG_NAME.StartsWith("v")) {
     $anaconda_label = "main"
-  } elseif ($Env:APPVEYOR_REPO_TAG_NAME.EndsWith("-beta")) {
+  } ElseIf ($Env:APPVEYOR_REPO_TAG_NAME.EndsWith("-beta")) {
     $anaconda_label = "beta"
-  } elseif ($Env:APPVEYOR_REPO_TAG_NAME.EndsWith("-dev")) {
+  } ElseIf ($Env:APPVEYOR_REPO_TAG_NAME.EndsWith("-dev")) {
     $anaconda_label = "dev"
-  } else {
+  } Else {
     Write-Host "Tag not for deployment, skipping conda package deployment."
     Return
   };
+
   Write-Host "anaconda_label $anaconda_label";
   Invoke-Expression "anaconda upload $file_to_upload -t $env:ANACONDA_TOKEN --label $anaconda_label --force"
-} else {
+  # Invoke-Expression "anaconda upload $tar_glob -t $env:ANACONDA_TOKEN --label $anaconda_label --force"
+} Else {
   Write-Host "Not tagged, skipping conda package deployment."
 }
