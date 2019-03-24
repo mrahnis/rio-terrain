13a58ddd95fc02d45b4b93b07b235ed6cae04511
Mike Rahnis <mike.rahnis@gmail.com>
2019-03-24 01:24:37 -0400

    Move the build artifacts where appveyor can get them

--- rio_terrain/__init__.py
+++ rio_terrain/__init__.py
@@ -5,7 +5,7 @@ from rio_terrain.core.statistics import *
 from rio_terrain.core.windowing import *
 
 __author__ = "Mike Rahnis"
-__version__ = '0.0.19'
+__version__ = '0.0.20'
 
 
 logger = logging.getLogger(__name__)
