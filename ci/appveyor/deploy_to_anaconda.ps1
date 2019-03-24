d6468c9e360e7de7eee3751b6027b2dd02ecc95b
Mike Rahnis <mike.rahnis@gmail.com>
2019-03-24 01:49:55 -0400

    Try to get around special characters

--- ci/appveyor/deploy_to_anaconda.ps1
+++ ci/appveyor/deploy_to_anaconda.ps1
@@ -25,8 +25,9 @@ If (($Env:APPVEYOR_REPO_TAG -eq "true") -and
   };
 
   Write-Host "anaconda_label $anaconda_label";
-  Invoke-Expression "anaconda -t $Env:ANACONDA_TOKEN upload $file_to_upload --label $anaconda_label --force"
-  # Invoke-Expression "anaconda upload $tar_glob -t $env:ANACONDA_TOKEN -u mrahnis --label $anaconda_label --force"
+  # if the token has certain special characters in it powershell sees subsequent -- as unary minus
+  Invoke-Expression "anaconda -t '$Env:ANACONDA_TOKEN' upload $file_to_upload --label $anaconda_label --force"
+  # Invoke-Expression "anaconda -t $env:ANACONDA_TOKEN upload $tar_glob --label $anaconda_label --force"
 } Else {
   Write-Host "Not tagged, skipping conda package deployment."
 }
--- rio_terrain/__init__.py
+++ rio_terrain/__init__.py
@@ -5,7 +5,7 @@ from rio_terrain.core.statistics import *
 from rio_terrain.core.windowing import *
 
 __author__ = "Mike Rahnis"
-__version__ = '0.0.20'
+__version__ = '0.0.21'
 
 
 logger = logging.getLogger(__name__)
