57ab67ffd1dd4e5283547820faab3fdaf7adfe34
Mike Rahnis <mike.rahnis@gmail.com>
2019-03-24 22:50:24 -0400

    Make anaconda labels a little nicer, add conda-verify to build reqs

--- ci/appveyor/deploy_to_anaconda.ps1
+++ ci/appveyor/deploy_to_anaconda.ps1
@@ -3,30 +3,25 @@ If (($Env:APPVEYOR_REPO_TAG -eq "true") -and
 
   $file_to_upload = (conda build --output .) | Out-String
 
-  #$tar_glob = ".\conda-bld\noarch\${Env:PYPKG}-${Env:APPVEYOR_REPO_TAG_NAME}-*.tar.bz2";
-  #Write-Host "distribution file: $tar_glob";
+  #$tar_glob = ".\conda-bld\noarch\${Env:PYPKG}-${Env:APPVEYOR_REPO_TAG_NAME}-*.tar.bz2"
+  #Write-Host "distribution file: $tar_glob"
 
-  #If (Test-Path $file_to_upload -PathType Leaf) {
-  #    Write-Host "Uploading $file_to_upload"
-  #} Else {
-  #    Write-Host "Unable to find file to upload: $file_to_upload" -ForegroundColor Red
-  #    Return
-  #}
-
-  If ($env:APPVEYOR_REPO_TAG_NAME.StartsWith("v")) {
+  If ($Env:APPVEYOR_REPO_TAG_NAME.StartsWith("v")) {
     $anaconda_label = "main"
-  } ElseIf ($Env:APPVEYOR_REPO_TAG_NAME.EndsWith("-beta")) {
-    $anaconda_label = "beta"
-  } ElseIf ($Env:APPVEYOR_REPO_TAG_NAME.EndsWith("-dev")) {
-    $anaconda_label = "dev"
-  } Else {
+
+    If ($Env:APPVEYOR_REPO_TAG_NAME.EndsWith("-beta")) {
+      $anaconda_label = "main beta"
+    } ElseIf ($Env:APPVEYOR_REPO_TAG_NAME.EndsWith("-dev")) {
+      $anaconda_label = "dev"
+    }
+  Else {
     Write-Host "Tag not for deployment, skipping conda package deployment."
     Return
-  };
+  }
 
-  Write-Host "anaconda_label $anaconda_label";
-  # if the token has certain special characters in it powershell sees subsequent -- as unary minus
-  Invoke-Expression "anaconda -t $Env:ANACONDA_TOKEN upload $file_to_upload -l $anaconda_label"
+  Write-Host "Uploading $file_to_upload with label $anaconda_label"
+  # if the token or filename has certain special characters in it powershell sees subsequent -- as unary minus
+  Invoke-Expression "anaconda -t $Env:ANACONDA_TOKEN upload --label $anaconda_label --force --no-progress $file_to_upload"
   # Invoke-Expression "anaconda -t $env:ANACONDA_TOKEN upload $tar_glob --label $anaconda_label --force"
 } Else {
   Write-Host "Not tagged, skipping conda package deployment."
