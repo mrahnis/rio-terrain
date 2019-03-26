1b1985e3fb139b1e37d2f074c9530a220e5e99c3
Mike Rahnis <mike.rahnis@gmail.com>
2019-03-25 20:52:38 -0400

    Finish tidying appveyor and conda build settings

--- ci/appveyor/deploy_to_anaconda.ps1
+++ ci/appveyor/deploy_to_anaconda.ps1
@@ -3,26 +3,20 @@ If (($Env:APPVEYOR_REPO_TAG -eq "true") -and
 
   $file_to_upload = (conda build --output .) | Out-String
 
-  #$tar_glob = ".\conda-bld\noarch\${Env:PYPKG}-${Env:APPVEYOR_REPO_TAG_NAME}-*.tar.bz2"
-  #Write-Host "distribution file: $tar_glob"
-
   If ($Env:APPVEYOR_REPO_TAG_NAME.StartsWith("v")) {
-    $anaconda_label = "main"
-
-    If ($Env:APPVEYOR_REPO_TAG_NAME.EndsWith("-beta")) {
-      $anaconda_label = "main beta"
-    } ElseIf ($Env:APPVEYOR_REPO_TAG_NAME.EndsWith("-dev")) {
-      $anaconda_label = "dev"
+    If ($Env:APPVEYOR_REPO_TAG_NAME.EndsWith("-dev")) {
+      $labels = "dev"
+    } ElseIf ($Env:APPVEYOR_REPO_TAG_NAME.EndsWith("-beta")) {
+      $labels = "beta"
+    } else {
+      $labels = "main"
     }
   } Else {
     Write-Host "Tag not for deployment, skipping conda package deployment."
     Return
   }
-
-  Write-Host "Uploading $file_to_upload with label $anaconda_label"
-  # if the token or filename has certain special characters in it powershell sees subsequent -- as unary minus
-  Invoke-Expression "anaconda -t $Env:ANACONDA_TOKEN upload --label $anaconda_label --force --no-progress $file_to_upload"
-  # Invoke-Expression "anaconda -t $env:ANACONDA_TOKEN upload $tar_glob --label $anaconda_label --force"
+  Write-Host "Deploying package $file_to_upload with label $labels"
+  Invoke-Expression "anaconda -t $Env:ANACONDA_TOKEN upload --label $labels --force --no-progress $file_to_upload"
 } Else {
   Write-Host "Not tagged, skipping conda package deployment."
 }
