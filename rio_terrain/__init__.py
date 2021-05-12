e0d5df027691eb3869b0c1eac899e95aaefc7b8b
Mike Rahnis <mike@topomatrix.com>
2021-05-12 16:53:15 -0400

    Tidy up after installing versioneer

--- rio_terrain/__init__.py
+++ rio_terrain/__init__.py
@@ -1,14 +1,13 @@
 import logging
+from ._version import get_versions
 
 from rio_terrain.core.terrain import *
 from rio_terrain.core.statistics import *
 from rio_terrain.core.windowing import *
 
-from ._version import get_versions
-
-__version__ = get_versions()['version']
-del get_versions
 
 __author__ = "Mike Rahnis"
+__version__ = get_versions()['version']
+del get_versions
 
 logger = logging.getLogger(__name__)
