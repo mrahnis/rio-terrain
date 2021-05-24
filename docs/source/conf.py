2b1b6c1e6945af595c177a2c50228dddff47aa15
Mike Rahnis <mike@topomatrix.com>
2021-05-24 14:20:46 -0400

    Switch autodoc_typehints back to description

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,7 +66,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 # fpr Napoleon to use the typehints
 napoleon_use_param = True
-autodoc_typehints = 'none'
+autodoc_typehints = 'description'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
